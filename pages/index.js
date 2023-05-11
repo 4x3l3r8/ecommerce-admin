0d32d111c9a58dd9b4cd6d88f0cb072ccb8f1c32
Adesanya Daniel <4x3l3r8@gmail.com>
2023-05-11 13:35:43 +0100

    feat: dashboard page, nav, products page, auth, db

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import Layout from "@/components/Layout"
+import Nav from "@/components/Nav"
+import { useSession, signIn, signOut } from "next-auth/react"
+import Image from "next/image"
+
 export default function Home() {
+  const { data: session } = useSession()
+
   return (
-    <>
-      <div className="bg-red-500">asdfa</div>
-    </>
+    <Layout>
+      <div className="flex justify-between text-blue-900">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex gap-1 overflow-hidden text-black bg-gray-300 rounded-lg">
+          <Image width={1200} height={1200} src={session?.user?.image} alt="user image" className="w-6 h-6" />
+          <span className="px-2">
+            {session?.user?.name}
+          </span>
+        </div>
+      </div>
+    </Layout>
   )
 }
